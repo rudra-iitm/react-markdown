723fe680bfa7c772531463edcddf4b83a4bf35d2
rudra-iitm <rudransh.iitm@gmail.com>
2024-12-30 01:00:48 +0530

    Add Component MarkdownRenderer

---
+++ components/MarkdownRenderer.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import remarkGfm from 'remark-gfm';
+
+interface MarkdownRendererProps {
+  content: string;
+}
+
+const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return !inline && match ? (
+            <SyntaxHighlighter
+              style={tomorrow}
+              language={match[1]}
+              PreTag="div"
+              {...props}
+            >
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
+      {content}
+    </ReactMarkdown>
+  );
+};
+
+export  { MarkdownRenderer };
---
+++ content/sample.md
@@ -0,0 +1,46 @@
+# Welcome to My Markdown Page
+
+This is a sample Markdown file that demonstrates various Markdown features.
+
+## Text Formatting
+
+You can make text **bold**, *italic*, or ***both***. You can also use ~~strikethrough~~.
+
+## Lists
+
+### Unordered List
+
+- Item 1
+- Item 2
+  - Subitem 2.1
+  - Subitem 2.2
+- Item 3
+
+### Ordered List
+
+1. First item
+2. Second item
+3. Third item
+
+## Links and Images
+
+Here's a [link to Google](https://www.google.com).
+
+And here's an image:
+
+![Next.js Logo](https://nextjs.org/static/favicon/favicon-32x32.png)
+
+## Code Blocks
+
+Here's some inline code: `const greeting = "Hello, world!";`
+
+And here's a code block:
+
+```sh
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(10));
+```
