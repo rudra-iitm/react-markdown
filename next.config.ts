cabc8cf87f212ada35c0fffeade50cb1b81bdfec
rudra-iitm <rudransh.iitm@gmail.com>
2024-12-30 01:04:12 +0530

    Update basePath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/open-printing",
+  basePath: "/react-markdown",
 };
 
 export default nextConfig;
